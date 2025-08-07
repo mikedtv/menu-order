6a77a3b8ade691f509be2508917bbd32e6d14440
mikedtv <md.vincent@gmail.com>
2025-08-07 17:31:35 +1000

    Added Code Coverage Reporting with vitest/coverage-v8

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,5 +14,8 @@ export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
   test: {
     environment: "happy-dom",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
   },
 });
